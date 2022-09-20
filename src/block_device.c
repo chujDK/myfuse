709e37981c9c636043b10010fbd589d9875ea8a8
chujDK <1768485949@qq.com>
2022-09-20 19:20:22 +0800

    fix: add lock to block device read and write

--- src/block_device.c
+++ src/block_device.c
@@ -1,15 +1,23 @@
 #include "block_device.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "util.h"
+
+// NOTE: after the read and write need be protected by locks as after the lseek
+// before the write the read-write position may be changed by other threads
+static pthread_mutex_t disk_lock;
 
 static int device_fd;
 
 static int write_block_raw_byfd(int fd, uint block_id, const u_char *buf) {
+  pthread_mutex_lock(&disk_lock);
   if (lseek(fd, block_id * BSIZE, SEEK_SET) != block_id * BSIZE) {
     return -1;
   }
-  return write(fd, buf, BSIZE);
+  int nbytes = write(fd, buf, BSIZE);
+  pthread_mutex_unlock(&disk_lock);
+  return nbytes;
 }
 
 int write_block_raw(uint block_id, const u_char *buf) {
@@ -17,10 +25,13 @@ int write_block_raw(uint block_id, const u_char *buf) {
 }
 
 static int read_block_raw_byfd(int fd, uint block_id, u_char *buf) {
+  pthread_mutex_lock(&disk_lock);
   if (lseek(fd, block_id * BSIZE, SEEK_SET) != block_id * BSIZE) {
     return -1;
   }
-  return read(fd, buf, BSIZE);
+  int nbytes = read(fd, buf, BSIZE);
+  pthread_mutex_unlock(&disk_lock);
+  return nbytes;
 }
 
 int read_block_raw(uint block_id, u_char *buf) {
@@ -29,13 +40,16 @@ int read_block_raw(uint block_id, u_char *buf) {
 
 static int read_block_raw_nbytes_byfd(int fd, uint block_id, u_char *buf,
                                       uint nbytes) {
+  pthread_mutex_lock(&disk_lock);
   if (lseek(fd, block_id * BSIZE, SEEK_SET) != block_id * BSIZE) {
     return -1;
   }
   if (nbytes > BSIZE) {
     nbytes = BSIZE;
   }
-  return read(fd, buf, nbytes);
+  int nbytes_read = read(fd, buf, nbytes);
+  pthread_mutex_unlock(&disk_lock);
+  return nbytes_read;
 }
 
 int read_block_raw_nbytes(uint block_id, u_char *buf, uint nbytes) {
@@ -47,4 +61,5 @@ void block_init(const char *path_to_device) {
   if (device_fd < 0) {
     err_exit("failed to open disk %s", path_to_device);
   }
+  pthread_mutex_init(&disk_lock, NULL);
 }
